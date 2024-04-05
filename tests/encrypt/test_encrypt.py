84ee05fd99df1cb500e34fdbb26452c352f9199b
Mandytrajano90 <mandytrajano90@gmail.com>
2024-04-05 17:00:36 -0300

    Second Requirement - feat encrypt test

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,15 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError) as error:
+        encrypt_message('Amanda', 'Carlos')
+    assert str(error.value) == 'The message must be a string'
+
+    with pytest.raises(TypeError) as error:
+        encrypt_message(4000, 30)
+    assert str(error.value) == 'The key must be a string'
+
+    assert encrypt_message("XFAIL", 99) == "LIAFX"
+    assert encrypt_message("HELLO", 3) == "KHOOR"
