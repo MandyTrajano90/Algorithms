615b33ec93abee7258d8a2ae493a0001fc7c87ec
Mandytrajano90 <mandytrajano90@gmail.com>
2024-04-05 16:20:01 -0300

    complexity fixed

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -13,6 +13,6 @@ def study_schedule(permanence_period, target_time):
         return None
     present_students = 0
     for student_time in permanence_period:
-        if target_time in range(student_time[0], student_time[1] + 1):
+        if target_time >= student_time[0] and target_time <= student_time[1]:
             present_students += 1
     return present_students
