2d3a4e285b9125fc9eb535192ed58d8e0cc302d2
Mandytrajano90 <mandytrajano90@gmail.com>
2024-04-05 16:00:22 -0300

    First Requirement - feat study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,18 @@
+def student_entry(entry) -> bool:
+    return (
+        isinstance(entry, tuple)
+        and len(entry) == 2
+        and all(isinstance(i, int) for i in entry)
+    )
+
+
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if any(
+        not student_entry(entry) for entry in permanence_period
+            ) or not isinstance(target_time, int):
+        return ValueError
+    present_students = 0
+    for student_time in permanence_period:
+        if target_time in range(student_time[0], student_time[1] + 1):
+            present_students += 1
+    return present_students
