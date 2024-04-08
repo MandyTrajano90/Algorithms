d2d3773c055e342ad32b29d98970ae81eac47182
Mandytrajano90 <mandytrajano90@gmail.com>
2024-04-08 09:46:21 -0300

    Sixth Requirement - feat palindromes_iterative

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,8 @@
-def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+def is_palindrome_iterative(word: str) -> bool:
+    if not word:
+        return False
+    size = len(word)
+    for i in range(0, size // 2):
+        if word[i] != word[size - i - 1]:
+            return False
+    return True
