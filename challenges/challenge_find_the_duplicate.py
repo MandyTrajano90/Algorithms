8b9967b431ec8cf00cc6769c078514b492d189fc
Mandytrajano90 <mandytrajano90@gmail.com>
2024-04-08 09:39:04 -0300

    Fifth Requirement - feat find_the_duplicate

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,6 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    nums.sort()
+    for i in range(1, len(nums)):
+        if nums[i] == nums[i - 1] and nums[i] > 0:
+            return nums[i]
+    return False
